eafe64da8fa7daf13b360074c18c2745ac5adcd0
Ângelo Hank <hankangelo6@gmail.com>
2025-11-20 15:12:45 -0300

    feat: criando rota e implementando insercao no cassandra

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import { pgRepository } from "./repositories/pg.repository";
+import { cassandraRepository } from "./repositories/cassandra.repository";
 const app = express();
 
 app.use(express.json());
@@ -24,6 +25,28 @@ app.post("/pg", async (req: Request, res: Response) => {
 
   try {
     const result = await pgRepository.insert(data);
+    return res.status(201).json({ message: "Data inserted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Failed to insert data" });
+  }
+});
+
+app.post("/cassandra", async (req: Request, res: Response) => {
+  const { sensor_id, value, metadata } = req.body;
+
+  if (!sensor_id || !value || !metadata) {
+    res.status(400).json({ error: "Missing required fields" });
+  }
+
+  const data = {
+    sensor_id,
+    value: parseFloat(value),
+    metadata: metadata || {},
+  };
+
+  try {
+    const result = await cassandraRepository.insert(data);
     return res.status(201).json({ message: "Data inserted successfully" });
   } catch (error) {
     console.log(error);
--- src/repositories/cassandra.repository.ts
+++ src/repositories/cassandra.repository.ts
@@ -1,7 +1,45 @@
 import { BaseRepository } from "./base.repository";
+import { Client, types } from "cassandra-driver";
+
+const client = new Client({
+  contactPoints: [process.env.CASSANDRA_HOST || "localhost"],
+  localDataCenter: process.env.CASSANDRA_DATACENTER || "dc1",
+  keyspace: process.env.CASSANDRA_KEYSPACE || "medicoes",
+  pooling: {
+    coreConnectionsPerHost: {
+      [types.distance.local]: 2,
+      [types.distance.remote]: 1,
+    },
+  },
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Cassandra connected");
+  })
+  .catch((err) => {
+    console.error("Cassandra connection error", err);
+  });
 
 export class CassandraRepository implements BaseRepository {
-  insert(data: any): Promise<any> {
-    return Promise.resolve(null);
+  async insert(data: any): Promise<any> {
+    const query = `INSERT INTO medicoes_test.medicoes (sensor_id, timestamp, id, value, metadata)
+                  VALUES (?, toTimestamp(now()), uuid(), ?, ?);`;
+    const params = [
+      data.sensor_id,
+      data.value,
+      data.metadata ? JSON.stringify(data.metadata) : {},
+    ];
+
+    try {
+      await client.execute(query, params, { prepare: true });
+      return { success: true, sensor_id: data.sensor_id };
+    } catch (error) {
+      console.error("Cassandra insert error", error);
+      throw error;
+    }
   }
 }
+
+export const cassandraRepository = new CassandraRepository();
